dc24adaf2cca48a33643cd05b70f446b7a90b2c7
Joshua Thompson <ysovuka@gmail.com>
2018-07-07 16:41:44 -0500

    Push to GitHub for saving.

---
+++ src/Application/Areas/Admin/Controllers/Api/CategoryApiController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace Application.Areas.Admin.Controllers.Api
+{
+    [Route("api/categories")]
+    public class CategoryApiController : Controller
+    {
+        private readonly ICategoryService _categoryService;
+        public CategoryApiController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        [HttpGet]
+        public IEnumerable<Category> GetAll(int? count, int? page)
+        {
+            if (count == null || page == null)
+            {
+                return _categoryService.GetAll();
+            }
+
+            return _categoryService.GetAllPAges(count, page);
+        }
+    }
+}
